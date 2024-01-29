19abe1f769cb147f0475e610473f6e649e919970
she110ff <youngsoo.j@gmail.com>
2024-01-29 16:04:42 +0900

    Upgrade client sdk

--- screens/initScreens/Permissions.js
+++ screens/initScreens/Permissions.js
@@ -55,7 +55,7 @@ const Permissions = observer(({ navigation }) => {
         userStore.setPermissionsCount();
       }
     } else {
-      navigation.navigate('Term');
+      navigation.navigate('InitPinCodeScreen');
     }
   }
   return (
