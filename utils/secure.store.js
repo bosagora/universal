4d1f398b91394004f73a0eac97a51c10339c86f5
she110ff <youngsoo.j@gmail.com>
2024-03-04 12:25:36 +0900

    Branch setting out from app kind

--- utils/secure.store.js
+++ utils/secure.store.js
@@ -1,4 +1,7 @@
 import * as SecureStore from 'expo-secure-store';
+import '@ethersproject/shims';
+import 'react-native-get-random-values';
+import { Wallet } from 'ethers';
 
 export async function saveSecureValue(key, value) {
   await SecureStore.setItemAsync(key, value);
@@ -13,4 +16,19 @@ export async function getSecureValue(key) {
     console.log('No values stored under that key.');
     return null;
   }
+}
+
+export async function saveSecure(key, secretStore, invalid) {
+  key = key.trim();
+  let wallet;
+  try {
+    wallet = new Wallet(key);
+  } catch (e) {
+    console.log('Invalid private key.');
+    alert(invalid);
+    return;
+  }
+  secretStore.setAddress(wallet.address);
+  await saveSecureValue('address', wallet.address);
+  await saveSecureValue('privateKey', key);
 }
